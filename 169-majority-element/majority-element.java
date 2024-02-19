201aac92dd8ea2687cf5d9d72fc1376e6020c316
suman-kumar24 <84268722+suman-kumar24@users.noreply.github.com>
2024-02-19 13:41:24 +0530

    Time: 1520 ms (5.01%) | Memory: 53.3 MB (6.56%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 0; i < nums.length ; i++)
         {
             int count = 0;
-            for(int j = 0; j<nums.length ; j++ )
+            for(int j = i; j<nums.length ; j++ )
             {
                 if(nums[i] == nums[j])
                     count++;
