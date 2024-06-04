f566d5646c59bb9b5da13205665637f61abd29cd
suman-kumar24 <84268722+suman-kumar24@users.noreply.github.com>
2024-06-04 10:06:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (36.89%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,13 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k =0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i] != val)
-            {
-                nums[k++] = nums[i]; 
+        int k = 0; // Index for the next element that is not 'val'
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i]; // Place element at index k and then increment k
             }
         }
-        return k;
+        return k; // k is the length of the array without 'val'
     }
 }
