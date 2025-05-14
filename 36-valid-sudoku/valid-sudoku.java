73712109de404b76e60338165e8e4bf5d79437b8
suman-kumar24 <84268722+suman-kumar24@users.noreply.github.com>
2025-05-14 17:23:36 +0530

    Time: 2 ms (78.78%) | Memory: 44.6 MB (50.57%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,20 +1,72 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen = new HashSet<>();
-
-        for(int i =0; i < 9; i++)
-            for(int j = 0; j < 9; j++)
-            {
-                char current = board[i][j];
-                if(current != '.')
-                if(!seen.add(current + " in row " + i) ||
-                    !seen.add(current + " in column " + j) ||
-                    !seen.add(current + " in sub-box " + i/3 + "-" + j/3))
-                {
+        // Check each row
+        for (int i = 0; i < 9; i++) {
+            if (!isValidRow(board, i)) {
+                return false;
+            }
+        }
+        
+        // Check each column
+        for (int j = 0; j < 9; j++) {
+            if (!isValidColumn(board, j)) {
+                return false;
+            }
+        }
+        
+        // Check each 3x3 sub-box
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValidSubBox(board, i, j)) {
                     return false;
                 }
             }
-
-        return true;    
+        }
+        
+        return true;
+    }
+    
+    private boolean isValidRow(char[][] board, int row) {
+        Set<Character> seen = new HashSet<>();
+        for (int j = 0; j < 9; j++) {
+            char current = board[row][j];
+            if (current != '.') {
+                if (seen.contains(current)) {
+                    return false;
+                }
+                seen.add(current);
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValidColumn(char[][] board, int col) {
+        Set<Character> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            char current = board[i][col];
+            if (current != '.') {
+                if (seen.contains(current)) {
+                    return false;
+                }
+                seen.add(current);
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValidSubBox(char[][] board, int startRow, int startCol) {
+        Set<Character> seen = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                char current = board[startRow + i][startCol + j];
+                if (current != '.') {
+                    if (seen.contains(current)) {
+                        return false;
+                    }
+                    seen.add(current);
+                }
+            }
+        }
+        return true;
     }
 }
