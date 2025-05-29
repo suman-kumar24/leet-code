b69e03c1d8151d651ffd126396d14551f7c258f6
suman-kumar24 <84268722+suman-kumar24@users.noreply.github.com>
2025-05-29 19:09:02 +0530

    Time: 31 ms (58.39%) | Memory: 51.4 MB (83.66%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,64 +1,28 @@
-class Solution {
+import java.util.*;
+
+public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // Sort the array to enable two-pointer technique
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        
-        // Initialize the list to store the result
-        List<List<Integer>> answer = new ArrayList<>();
-        
-        // If the array has less than 3 elements, return the empty list
-        if (nums.length < 3) {
-            return answer;
-        }
-        
-        // Iterate through the array, treating each element as the first element of the triplet
-        for (int i = 0; i < nums.length; i++) {
-            // Since the array is sorted, if the current element is greater than 0, 
-            // further elements will also be greater than 0, making the sum > 0.
-            if (nums[i] > 0) {
-                break;
-            }
-            
-            // Skip duplicate elements to avoid duplicate triplets in the result
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            
-            // Initialize two pointers for the two-sum problem
-            int low = i + 1, high = nums.length - 1;
-            
-            // Use two-pointer technique to find pairs that sum up to -nums[i]
-            while (low < high) {
-                int sum = nums[i] + nums[low] + nums[high];
-                
-                if (sum > 0) {
-                    // If the sum is greater than 0, move the high pointer to the left
-                    high--;
-                } else if (sum < 0) {
-                    // If the sum is less than 0, move the low pointer to the right
-                    low++;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+                if (total == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (total < 0) {
+                    left++;
                 } else {
-                    // If the sum is 0, we found a triplet
-                    answer.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                    
-                    // Store the current low and high values to skip duplicates
-                    int lastLowOccurrence = nums[low];
-                    int lastHighOccurrence = nums[high];
-                    
-                    // Skip duplicate values for the low pointer
-                    while (low < high && nums[low] == lastLowOccurrence) {
-                        low++;
-                    }
-                    
-                    // Skip duplicate values for the high pointer
-                    while (low < high && nums[high] == lastHighOccurrence) {
-                        high--;
-                    }
+                    right--;
                 }
             }
         }
-        
-        // Return the list of triplets
-        return answer;
+        return res;
     }
 }
